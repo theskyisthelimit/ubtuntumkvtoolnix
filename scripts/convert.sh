1d88b221baac3aa9708cdbfbf70089ada283311f
theskyisthelimit <pbognar@gmail.com>
2020-01-04 17:29:40 +0100

    verbesserungen & fixes

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,35 +1,34 @@
-#for file in "$@"*.mkv; do
-if [[ "${sonarr_eventtype}" = "Test" ]];
-then
-  echo "nothing to see here"
-  exit
-else
-    for file in "$1";
-    do
-      subs=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: subtitles (SubRip\/SRT).* language:[eg].*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
-      audio=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: audio.* language:[eg].*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
+#!/bin/bash
+echo $1
+cd "$1"
+for file in *.mkv;
+do
+  subs=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: subtitles (SubRip\/SRT).* language:[eg].*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
+  audio=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: audio.* language:[egj].*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
 
-        if [ -z "$subs" ]
-        then
+    if [ -z "$subs" ]
+    then
 
-          echo "Nothing to remove, will look for ASS Files"
-          subs=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: subtitles (SubStationAlpha).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
-          echo "found $subs to remove"
-          audio=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: audio.* language:[eg].*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
-          echo "found $audio to keep"
+      echo "Nothing to remove, will look for ASS & PGS Files"
+      audio=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: audio.* language:[egj].*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
+      echo "found $audio to keep"
+      subs=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: subtitles [(SubStationAlpha)|(HDMV/PGS)|(VobSub)].*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
+      echo "found $subs to remove"
 
-          subs="-S";
-          audio="-a $audio";
+      subs="-S";
+      audio="-a $audio";
 
-          mkvmerge $subs $audio -o "${file%.mkv}".edited.mkv "$file";
-          mv "$1" /media/Trash/;
-        else
-          subs="-s $subs";
-          audio="-a $audio";
+      mkvmerge $subs $audio -o "${file%.mkv}".edited.mkv "$file";
+      mv "${file%.mkv}".edited.mkv "$file"
+      # mv "$1" /media/Trash/;
+    else
+      subs="-s $subs";
+      audio="-a $audio";
 
-          mkvmerge $subs $audio -o "${file%.mkv}".edited.mkv "$file";
-          echo "Remove"
-          mv "$1" /media/Trash/;
-        fi
+      mkvmerge $subs $audio -o "${file%.mkv}".edited.mkv "$file";
+      echo "Remove"
+      mv "${file%.mkv}".edited.mkv "$file"
+      # mv "$1" /media/Trash/;
+    fi
     done
-fi
+exit
