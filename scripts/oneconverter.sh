f474662514255b668c532f9454e378e155bd77f5
theskyisthelimit <pbognar@gmail.com>
2020-01-12 15:40:05 +0100

    update

--- scripts/oneconverter.sh
+++ scripts/oneconverter.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-echo $1
-cd "$1"
-for file in *.mkv;
+for file in "$1"
 do
   audio=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: audio .* language:\(ger\|eng\).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
-  echo "found $audio to keep"
+  echo "found $audio audio to keep"
   subs=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: subtitles (SubRip\/SRT).* language:\(ger\|eng\).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
-  echo "found $subs to keep"
+  echo "found $subs subs to keep"
 
     if [ -z "$subs" ]
     then
 
       echo "Nothing to remove, will look for ASS & PGS Files"
       audio=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: audio .* language:\(ger\|eng\).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
-      echo "found $audio to keep"
+      echo "found $audio audio to keep"
       subs=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: subtitles [(SubStationAlpha)|(HDMV/PGS)|(VobSub)].*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
-      echo "found $subs to remove"
+      echo "found $subs subs to keep"
 
       subs="-S";
       audio="-a $audio";
