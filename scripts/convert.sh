71fcafd4b2524a581fe74fe9a21b56dbed75da23
theskyisthelimit <pbognar@gmail.com>
2020-01-20 07:57:53 +0100

    working scripts

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -3,7 +3,7 @@ echo $1
 cd "$1"
 for file in *.mkv;
 do
-  audio=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: audio .* language:\(ger\|eng\).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
+  audio=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: audio .* language:\(ger\|eng\|jpn\).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
   echo "found $audio to keep"
   subs=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: subtitles (SubRip\/SRT).* language:\(ger\|eng\).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
   echo "found $subs to keep"
@@ -12,7 +12,7 @@ do
     then
 
       echo "Nothing to remove, will look for ASS & PGS Files"
-      audio=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: audio .* language:\(ger\|eng\).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
+      audio=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: audio .* language:\(ger\|eng\|jpn\).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
       echo "found $audio to keep"
       subs=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: subtitles [(SubStationAlpha)|(HDMV/PGS)|(VobSub)].*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
       echo "found $subs to remove"
--- scripts/subtitlefinder.sh
+++ scripts/subtitlefinder.sh
@@ -3,6 +3,7 @@ for file in "$1"
 do
   echo -e "\033[31m $1"'\033[0m' >> /media/scripts/logfile.log
   mkvmerge -I "$file" | grep 'SubStationAlpha\|HDMV\/PGS\|VobSub' >> /media/scripts/logfile.log
+  mkvmerge -I "$file" | grep 'sAtmos' >> /media/scripts/logfile.log
 #  mkvmerge -I "$file" | grep 'SubRip' >> /media/scripts/logfile.log
   echo >> /media/scripts/logfile.log
 done
