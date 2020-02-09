5b58a6497dbe7be98d19b8b183fac1539640ad32
theskyisthelimit <pbognar@gmail.com>
2020-02-09 15:19:02 +0100

    remove scripts

--- scripts/batchfinder.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -f /media/scripts/logfile.log ]]; then
-  rm /media/scripts/logfile.log
-fi
-find "`pwd`" -type f -name "*.mkv" -size +1G -printf '\033[32m%p\033[0m\n'
-find "`pwd`" -type f -name "*.mkv" -size +1G -exec /media/scripts/subtitlefinder.sh {} \;
--- scripts/subtitlefinder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for file in "$1"
-do
-  echo -e "\033[31m $1"'\033[0m' >> /media/scripts/logfile.log
-  mkvmerge -I "$file" | grep 'SubStationAlpha\|HDMV\/PGS\|VobSub' >> /media/scripts/logfile.log
-  mkvmerge -I "$file" | grep 'sAtmos' >> /media/scripts/logfile.log
-#  mkvmerge -I "$file" | grep 'SubRip' >> /media/scripts/logfile.log
-  echo >> /media/scripts/logfile.log
-done
