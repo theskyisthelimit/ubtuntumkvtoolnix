9b64a11a17e81eee8252d072f8f370d1ca76e96a
mat3280 <46890527+mat3280@users.noreply.github.com>
2020-04-08 19:45:37 +0200

    optimisation of the script

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -2,59 +2,47 @@
 mkv () {
   if [ ! -z "$1" ]
   then
-   echo "Starting conversion for $1"
-   file=$(echo "$1")
-   mkvmerge -I "$file"
-   audio=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: audio .* language:\(ger\|eng\|jpn\|und\).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
-   audiocount=$(echo $audio | tr "," "\n" | wc -l)
-   echo "1: found $audio ($audiocount) to keep"
-   subs=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: subtitles (SubRip\/SRT).* language:\(ger\|eng\).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
-   subscount=$(echo $subs | tr "," "\n" | wc -l)
-   echo "2: found $subs ($subscount) to keep"
-
-   totalaudio=$(mkvmerge -I "$file" | grep audio | wc -l)
-   totalsubs=$(mkvmerge -I "$file" | grep subtitles | wc -l)
+    echo "Starting conversion for $1"
+    file=$(echo "$1")
+    mkvmerge -I "$file"
+    audio=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: audio .* language:\(ger\|eng\|jpn\|und\).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
+    audiocount=$(echo $audio | tr "," "\n" | wc -l)
+    echo "1: found $audio ($audiocount) to keep"
+    
+	subs=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: subtitles (SubRip\/SRT).* language:\(ger\|eng\).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
+    subscount=$(echo $subs | tr "," "\n" | wc -l)
+    echo "2: found $subs ($subscount) to keep"
+        
+    totalaudio=$(mkvmerge -I "$file" | grep audio | wc -l)
+    totalsubs=$(mkvmerge -I "$file" | grep subtitles | wc -l)
   
-   diffaudio=$(expr $totalaudio - $audiocount)
-   diffsubs=$(expr $totalsubs - $subscount)
-  
-     if [ -z "$subs" ]
-     then
-       echo "3: Nothing to remove, will look for ASS & PGS Files"
-       audio=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: audio .* language:\(ger\|eng\|jpn\|und\).*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
-       echo "4: found $audio to keep"
-       subs=$(mkvmerge -I "$file" | sed -ne '/^Track ID [0-9]*: subtitles [(SubStationAlpha)|(ASS)|(HDMV/PGS)|(VobSub)].*/ { s/^[^0-9]*\([0-9]*\):.*/\1/;H }; $ { g;s/[^0-9]/,/g;s/^,//;p }')
-       echo "5: found $subs to remove"
+    diffaudio=$(expr $totalaudio - $audiocount)
+    diffsubs=$(expr $totalsubs - $subscount)
 
-       if [ -z "$subs" ]
-       then
-         echo "6: Nothing found to remove. Will exit script now."
+    echo "3: setting parameters"
 
-       else
-         subs="-S";
-         audio="-a $audio";
-         mkvmerge $subs $audio -o "${file%.mkv}".edited.mkv "$file";
-         mv "${file%.mkv}".edited.mkv "$file"
-         echo "7: PGS/ASS/VobSub Subtitles found and removed!"
-         # mv "$1" /media/Trash/;
-       fi
+    if [ -z "$subs" ]
+    then
+      subs="-S"
+    else
+      subs="-s $subs"
+    fi
+    
+	audio="-a $audio"
 	
-	 elif [ $diffsubs -eq 0 -a $diffaudio -eq 0 ]
-	 then
-	   echo "8: Only needed audio and subtitles found" 
-	  
-     else
-       echo "8: Found Subtitles. Will multiplex now"
-       subs="-s $subs";
-       audio="-a $audio";
-
-       mkvmerge $subs $audio -o "${file%.mkv}".edited.mkv "$file";
-       mv "${file%.mkv}".edited.mkv "$file"
-       # mv "$1" /media/Trash/;
-     fi
+    if [ $diffaudio -gt 0 -o $diffsubs -gt 0 ]
+	then
+	  mkvmerge $subs $audio -o "${file%.mkv}".edited.mkv "$file";
+	  mv "${file%.mkv}".edited.mkv "$file"
+	  echo "4: Unwanted audio or subtitles found and removed!"		
+	  # mv "$1" /media/Trash/;
+			
+	else
+	  echo "4: Nothing found to remove. Will exit script now."
+	fi
 	
   else
-	echo "9: Nothing to do... exiting function"
+	echo "5: Nothing to do... exiting function"
   fi	 
 }
 
@@ -71,7 +59,7 @@ then
   mkv "$1"
 
 else
-  echo "9: Not a file nor a directory... exiting"
+  echo "6: Not a file nor a directory... exiting"
   exit
 fi
 exit
